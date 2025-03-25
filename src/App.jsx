6858e3da1621827639c1d48decaebae6e090b05f
Abhi7033 <abhishekanand3860@gmail.com>
2025-03-25 15:54:09 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 function App() {
   return <div>
-    <b>
+    {/* <b>
     Hii there
-    </b>
+    </b> */}
     <Counter></Counter>
   </div>
 }
@@ -14,23 +14,37 @@ function Counter(){
 
   const [count, setCount] = useState(0);
 
-  function increaseCount(){
-    setCount( count + 1 );
-  }
+  //here if we put setInterval here then it will log counter in a crazy way because
+  //when the state changes react rerender its component so there is a concept 
+  //hooking into the lifecycle events of react (mounting, re-rendering, unmounting)
+  //now we want whenever counter component mounts then we want to run the setInterval code
+  //for that we use USEEFFECT
 
-  function decreaseCount(){
-    setCount( count - 1 );
-  }
+  console.log("counter");
 
-  function resetCount(){
-    setCount(0);
-  }
+  //guard our setInterval from re-renders
+  useEffect(function(){
+    setInterval( function(){
+      setCount(count => count + 1 );
+    },1000)
+    console.log("mounted");
+    
+  },[]);
+  
+  // function increaseCount(){
+  //   setCount( count + 1 );
+  // }
+
+  // function decreaseCount(){
+  //   setCount( count - 1 );
+  // }
+
+  // function resetCount(){
+  //   setCount(0);
+  // }
 
   return <div>
-    <h1>{count}</h1>
-    <button onClick={increaseCount}>Increase Count</button>
-    <button onClick={decreaseCount}>Decrease Count</button>
-    <button onClick={resetCount}>Reset Count</button>
+    <h1 id="text">{count}</h1>
   </div>
 }
 
